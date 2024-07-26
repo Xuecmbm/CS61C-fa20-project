dd96aaf7aaccb22c29c2a00c48a302ede7096039
baomihua <1124653720@qq.com>
2024-07-26 15:43:18 +0800

    finish project1 Part A1

--- project1/imageloader.c
+++ project1/imageloader.c
@@ -26,16 +26,90 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 0;
+    }
+
+    char header[3];
+    int width, height, maxval;
+
+    // Read and check the header
+    if (fgets(header, sizeof(header), file) == NULL) {
+        printf("Failed to read file header\n");
+        fclose(file);
+        return 1;
+    }
+    if (strncmp(header, "P3", 2) != 0) {
+        printf("Invalid file format: %s\n", header);
+        fclose(file);
+        return 1;
+    }
+
+    // Read the next three integers
+    if (fscanf(file, "%d %d %d", &width, &height, &maxval) != 3) {
+        printf("Failed to read width, height, and maxval\n");
+        fclose(file);
+        return 1;
+    }
+    
+    Image *image = (Image *)malloc(sizeof(Image));
+    image->rows = width;
+    image->cols = height;
+    image->image = (Color **)malloc(sizeof(Color *) * width * height);
+    Color **now = image->image;
+    
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            Color* p = (Color *)malloc(sizeof(Color));
+            int r, g, b;
+            fscanf(file, "%d %d %d", &r, &g, &b);
+            p->R = r;
+            p->G = g;
+            p->B = b;
+            *now = p;
+            now++;
+        }
+    }
+
+    fclose(file);
+    return image;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+    int width = image->cols, height = image->rows;
+    printf("P3\n%d %d\n255\n", width, height);
+    Color **color = image->image;
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+	    Color *temp = *color;
+	    color++;
+	    if(j != 0)printf("   ");
+	    printf("%3d %3d %3d", temp->R, temp->G, temp->B);
+	}
+	printf("\n");
+    }
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+    int width = image->cols, height = image->rows;
+    Color **color = image->image;
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+	    Color *temp = *color;
+	    free(temp);
+	    color++;
+	}
+    }
+    free(image->image);
+    free(image);
 }
